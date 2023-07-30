64073a9b65a7bde483abdcecca7c3903dd66861c
Aleksandr Kross <krossale@fel.czut.cz>
2023-07-30 16:47:44 +0200

    Upgraded Player from 30 July 2023

--- src/main/java/cz/cvut/fel/pjv/entity/Player.java
+++ src/main/java/cz/cvut/fel/pjv/entity/Player.java
@@ -155,7 +155,15 @@ public class Player extends Entity {
     public void interactNPC(int i) {
         if (i != 999) {
 
-            System.out.println("u are hitting an npc !");
+            if (i != 999) {
+
+                if (gp.keyH.enterPressed == true) {
+
+                    gp.gameState = gp.dialogueState;
+                    gp.npc[i].speak();
+                }
+            }
+            gp.keyH.enterPressed = false;
         }
 
     }
