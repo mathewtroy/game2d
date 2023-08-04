5ab6d566533cc3be759b1a37300371d352440c13
Aleksandr Kross <krossale@fel.czut.cz>
2023-08-04 23:09:32 +0200

    Created Inventory Screen.
    Added Cursor to Inventory Screen
    Added Moving Cursor.
    Added Weapon, Shield to Inventory.
    Added description of Item in Inventory.

--- src/main/java/cz/cvut/fel/pjv/object/OBJ_Key.java
+++ src/main/java/cz/cvut/fel/pjv/object/OBJ_Key.java
@@ -12,6 +12,8 @@ public class OBJ_Key extends Entity {
 
         name = "Key";
         down1 = setup("/objects/key", gp.tileSize, gp.tileSize);
+        description = "[" + name + "]\nAn old key.\nMade in China.";
+
 
     }
 
